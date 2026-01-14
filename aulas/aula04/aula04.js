001fcccec6d4830234801ca443a6c294ef6ff62d
Ray Camurça <raycamurca7@gmail.com>
2026-01-14 15:29:21 -0400

    up aula04

---
+++ aulas/aula04/aula04.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let n1 = 0, n2 = 0, resp = 0;
+
+n1 = 10;
+
+n1 /= 5;
+
+console.log("o valor de n1 é: " + n1);
+
+/*
++ (adição)
+- (subtração)
+* (multiplicação)
+/ (divisão)
+** (potenciação)
+% (resto da divisão)
+++ ou +=  (incremento)
+-- ou -= (decremento)
+*/
