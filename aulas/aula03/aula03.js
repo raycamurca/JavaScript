aabb39db04f653a3041c20d644b4332a120707cf
Ray Camurça <raycamurca7@gmail.com>
2026-01-14 14:20:16 -0400

    up aula03

---
+++ aulas/aula03/aula03.js
@@ -0,0 +1,11 @@
+    "use strict";
+if (true) {
+    var nome = "opa";
+    console.log ("dentro do if " + nome);
+}
+
+    console.log (nome);
+
+    const curso = "JavaScript";
+
+    console.log (curso);
