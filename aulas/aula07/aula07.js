f8cea7dfcdded1f9988031c0c486a6e3adeb961a
Ray Camurça <raycamurca7@gmail.com>
2026-01-15 10:34:00 -0400

    create aula07

---
+++ aulas/aula07/aula07.js
@@ -0,0 +1,16 @@
+
+let n1 = 10;
+let n2 = 5;
+
+if ((n1>n2) && (n1<n2) ){
+    console.log ("VVVVVV");
+} else {
+    console.log ("FFFFFF")
+}
+
+
+/* 
+&& (e / and)
+|| (ou / or)
+! (não / not)
+*/
