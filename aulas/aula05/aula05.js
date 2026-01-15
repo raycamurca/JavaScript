e13dee3f362e21f794246f02432249613f6ba44d
Ray Camurça <raycamurca7@gmail.com>
2026-01-15 09:21:25 -0400

    up aula05

---
+++ aulas/aula05/aula05.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let n1 = 10;
+let n2 = 5;
+let n3 = 10;
+
+console.log (n1!=n3);
+
+
+
+
+/*
+> (maior)
+>= (maior ou igual)
+< (menor)
+<= (menor ou igual)
+== (igual)
+!= (diferente)
+*/
+
+// = (atribuição) variável recebe um valor.
