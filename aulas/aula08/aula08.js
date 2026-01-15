a69cf97bfeb3a503a81b3df9e63091721d9cc787
Ray Camurça <raycamurca7@gmail.com>
2026-01-15 14:33:12 -0400

    up aula08

---
+++ aulas/aula08/aula08.js
@@ -0,0 +1,19 @@
+
+let n1 = 10;
+
+let res = n1 >>1;
+
+console.log (res);
+
+
+/* 
+número impar ou par.
+
+let n = 232297503;
+
+if (n % 2== 0) {
+    console.log ("O número " + n + " é par")
+} else {
+    console.log ("O número " + n + " é impar")
+}
+*/
