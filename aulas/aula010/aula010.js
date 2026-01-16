a223e17c49dbe9752ea0c12136aaff4c3c851d27
Ray Camurça <raycamurca7@gmail.com>
2026-01-16 12:35:14 -0400

    create aula010

---
+++ aulas/aula010/aula010.js
@@ -0,0 +1,32 @@
+let st = "A";
+
+x= (st == "A") ? "Ativo" : "Inativo";
+
+console.log ("Status do Usuário: " + x);
+
+// condição ? se Verdadeiro : se falso
+
+/*
+Com If/Else:
+
+let res = n1%2;
+
+if (res ==0) {
+    console.log ("par")
+} else {
+    console.log ("impar")
+}
+
+Com operador ternário:
+
+let n1 = 10;
+res = (n1 % 2 == 0 ? "par" : "impar");
+console.log (res);
+
+
+let idade = 18;
+
+x = idade >=18 ? "maior de idade" : "menor de idade";
+
+console.log (x);
+*/
