070fceac37b5977fb5b8f2cc404874785df7611a
Ray Camurça <raycamurca7@gmail.com>
2026-01-17 09:03:56 -0400

    create aula013

---
+++ aulas/aula013/aula013.js
@@ -0,0 +1,11 @@
+let nota = 7;
+
+if(nota>=7) {
+    console.log("Aprovado");
+} else if(n>=5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
+
+console.log ("fim do programa");
