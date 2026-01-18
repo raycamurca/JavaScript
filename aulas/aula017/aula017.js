3e2dc61448847369f7f611a3823f44e4aa0b49c3
Ray Camurça <raycamurca7@gmail.com>
2026-01-18 11:18:02 -0400

    create aula017

---
+++ aulas/aula017/aula017.js
@@ -0,0 +1,24 @@
+    let n =7;
+    let fat=1;
+
+    while(n>=1) {
+        fat*=n //fat= fat*n
+        n-- // n= n-1
+    }
+    
+    console.log(fat)
+
+    /*console.log("inicio do programa")
+    
+    let n = 5;
+    let r = 0;
+    let resp = 1;
+
+    while(n>=1) {
+        r = n;
+        resp = r*1*resp
+        console.log(n)
+        n--
+    }
+
+    console.log("o fatorial é igual a: " + resp)*/
