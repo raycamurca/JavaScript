c183f4ed3d7a20a135eb557aa3e667730851b82a
Ray Camurça <raycamurca7@gmail.com>
2026-01-18 14:23:41 -0400

    create aula019

---
+++ aulas/aula019/aula019.js
@@ -0,0 +1,16 @@
+    let n = 0;
+    let max = 100;
+    let par = 0;
+
+    for (let i=n; i<max; i++) {
+        if(i%2!=0) {
+            continue
+        }
+        par++
+    }
+
+    console.log("qtd de pares: " + par)
+    console.log("fim do programa")
+
+
+
