c7f54bed5b9cf83dfcf99f0a4aeb1dc6ea5af481
Ray Camurça <raycamurca7@gmail.com>
2026-01-19 12:20:54 -0400

    create aula020

---
+++ aulas/aula020/aula020.js
@@ -0,0 +1,12 @@
+// Declaração da função (criação)
+function mudarTexto() {
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="manipulação do DOM"
+    d2.innerHTML="manipulação do DOM"
+    d3.innerHTML="manipulação do DOM"
+}
+
+//chamada da função
+//nome()
