1ae3bd64cf738a86248951028d15ba4e3e38eb2e
Ray Camurça <raycamurca7@gmail.com>
2026-01-21 13:44:06 -0400

    create aula028

---
+++ aulas/aula028/aula028.js
@@ -0,0 +1,20 @@
+let pessoas = [{nome:"rayzin", idade: 19}, {nome:"jhennifer", idade:18}]
+let nome = pessoas.map(n=>n.nome)
+let idade = pessoas.map(i=>i.idade)
+
+console.log(nome);
+console.log(idade);
+
+// let primos =[2, 3, 5, 7, 11];
+// let dobroPrimos= primos.map(n=> n*2)
+
+// console.log(dobroPrimos)
+
+
+
+/*
+const cursos= ['HTML','CSS','Javascript','PHP','React'];
+cursos.map((elemento,posição)=>{
+    console.log("Curso: " + elemento + " - Posição do curso: "+ posição)
+})
+*/
