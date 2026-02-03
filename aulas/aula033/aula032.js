6ede37412f347d55082a74d5e97493f325c8856f
Ray Camurça <raycamurca7@gmail.com>
2026-02-03 13:09:29 -0400

    up

---
+++ aulas/aula033/aula032.js
@@ -0,0 +1,24 @@
+const divTodas=[...document.getElementsByTagName("div")]
+
+const cursoTodos = [...document.getElementsByClassName("curso")]
+
+const cursosc1 = [...document.getElementsByClassName("c1")]
+
+const cursosc2 = [...document.getElementsByClassName("c2")]
+
+const cursoEspecial=document.getElementById("c1")
+const querySelect=[...document.querySelectorAll("div")]
+
+console.log(querySelect);
+
+
+// console.log(divTodas);
+// console.log(cursoTodos);
+// console.log(cursosc1);
+// console.log(cursosc2);
+// console.log(cursoEspecial);
+
+
+// cursosc1.map(el=>{
+//     el.classList.add("destaque")
+// })
