ca1a6505d6fa5934be55e3425ef1009f6ec4dc07
Ray Camurça <raycamurca7@gmail.com>
2026-03-05 10:28:47 -0400

    UP

---
+++ aulas/aula038/script.js
@@ -0,0 +1,9 @@
+const divPai = document.querySelector(".cursos-disponiveis");
+const cursos = [...document.querySelectorAll(".curso")];
+
+// console.log(divPai.hasChildNodes());
+// console.log(cursos[0].hasChildNodes());
+
+const mudar = divPai.children[0];
+
+mudar.innerHTML=`OPA`
